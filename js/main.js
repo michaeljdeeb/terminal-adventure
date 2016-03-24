9444115c2090eb817c79217a5a1d4e435eace671
Michael Deeb <michaeljdeeb@gmail.com>
2016-03-24 13:05:41 -0700

    Fixed a minor bug with deletion

--- js/main.js
+++ js/main.js
@@ -1,38 +1,42 @@
-var commandStart = 'Linux-Pro:~ admin$ '
-// Find a way to disable the ability to delete the login line stuff
+(function () {
+  var commandStart = 'Terminal-Adventure:~ admin$ '
+  // Find a way to disable the ability to delete the login line stuff
 
 
-// Add the block character as the text cursor
+  // Add the block character as the text cursor
 
-// Write the logic to verify correct commands
-var terminal = document.querySelector('textarea.terminal-content');
-// terminal.addEventListener('keyup', function(e) {
-terminal.addEventListener('keydown', function(e) {
-  var terminalContent = terminal.value;
-  // console.dir(e);
+  // Write the logic to verify correct commands
+  var terminal = document.querySelector('textarea.terminal-content');
+  terminal.value = commandStart;
+  
+  // terminal.addEventListener('keyup', function(e) {
+  terminal.addEventListener('keydown', function(e) {
+    var terminalContent = terminal.value;
+    // console.dir(e);
 
-  if(e.keyCode === 13) { // enter
-    e.preventDefault();
-    terminal.value = terminalContent + '\n' + commandStart;
-  }
-  if(e.keyCode === 67 && e.ctrlKey) { // ctrl+c
-    // don't process input, but create new line
-    // print ^C
-  }
-  if(e.keyCode === 8) { // backspace
-    e.preventDefault();
-
-    // Split the window on returns
-    var terminalLines = terminalContent.split('\n');
-
-    // Look at the last split
-    var lastSplit = terminalLines[terminalLines.length - 1];
-    if (lastSplit.length > commandStart.length) {
-      terminal.value = terminalContent.substring(0, terminalContent.length - 1);
+    if(e.keyCode === 13) { // enter
+      e.preventDefault();
+      terminal.value = terminalContent + '\n' + commandStart;
     }
-  }
-}, false);
+    if(e.keyCode === 67 && e.ctrlKey) { // ctrl+c
+      // don't process input, but create new line
+      // print ^C
+    }
+    if(e.keyCode === 8) { // backspace
+      e.preventDefault();
 
-// v2 Allow partial correct solutions
+      // Split the window on returns
+      var terminalLines = terminalContent.split('\n');
 
-// Minify JS to obscure answers
+      // Look at the last split
+      var lastSplit = terminalLines[terminalLines.length - 1];
+      if (lastSplit.length > commandStart.length) {
+        terminal.value = terminalContent.substring(0, terminalContent.length - 1);
+      }
+    }
+  }, false);
+
+  // v2 Allow partial correct solutions
+
+  // Minify JS to obscure answers
+})();
